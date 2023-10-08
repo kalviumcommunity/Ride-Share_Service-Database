b0767606aefd1ccbe73a7011157fe62858d66980
BalajiSriraman <balajisr000@gmail.com>
2023-10-08 22:31:03 +0530

    chore: fix the tables and are normalised

--- SQL scripts/tables.sql
+++ SQL scripts/tables.sql
@@ -1,9 +1,9 @@
 CREATE TABLE Driver (
     DriverId INT PRIMARY KEY,
-    DriverRating DECIMAL(1),
+    DriverRating DECIMAL(3, 2), -- Adjusted precision and scale
     Name VARCHAR(255),
     PhoneNo VARCHAR(15),
-  	VehicleInfo VARCHAR(255),
+    VehicleInfo VARCHAR(255),
     UserId INT,
     FOREIGN KEY (UserId) REFERENCES User(UserId)
 );
@@ -16,30 +16,21 @@ CREATE TABLE Ride (
     RideStartTime DATETIME,
     RideStatus VARCHAR(20),
     UserId INT,
+    PickupLocation VARCHAR(255),
+    DropoffLocation VARCHAR(255),
     FOREIGN KEY (DriverId) REFERENCES Driver(DriverId),
     FOREIGN KEY (UserId) REFERENCES User(UserId)
-);
-
-CREATE TABLE Location (
-    LocationId INT PRIMARY KEY,
-    DropOffLocation VARCHAR(255),
-    PickupLocation VARCHAR(255),
-    RideId INT, -- Add this column
-    Address VARCHAR(255),
-     UserId INT,
-    FOREIGN KEY (UserId) REFERENCES User(UserId)
-    FOREIGN KEY (RideId) REFERENCES Ride(RideId) -- Corrected foreign key definition
 );
 
 CREATE TABLE Payment (
     PaymentId INT PRIMARY KEY,
-    DriverId INT,
     PaymentMethod VARCHAR(50),
     PaymentStatus VARCHAR(20),
     Price DECIMAL(8, 2),
     UserId INT,
-    FOREIGN KEY (DriverId) REFERENCES Driver(DriverId),
-    FOREIGN KEY (UserId) REFERENCES User(UserId)
+    FOREIGN KEY (UserId) REFERENCES User(UserId),
+    RideId INT,
+    FOREIGN KEY (RideId) REFERENCES Ride(RideId)
 );
 
 CREATE TABLE User (
