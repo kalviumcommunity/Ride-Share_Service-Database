300022bfd2803493f1d1f0183057ef44993adda9
Balaji S <balajisr000@gmail.com>
2023-10-15 07:40:14 +0530

    fix postgres table

--- SQL scripts/tables.sql
+++ SQL scripts/tables.sql
@@ -1,13 +1,3 @@
--- Create the Ride table
-CREATE TABLE "User" (
-    UserId SERIAL PRIMARY KEY,
-    Email VARCHAR(255) NOT NULL,
-    Name VARCHAR(255) NOT NULL,
-    Password VARCHAR(255) NOT NULL,
-    PhoneNumber VARCHAR(20) NOT NULL,
-    Role VARCHAR(50) NOT NULL
-);
--- Create the Driver table
 CREATE TABLE Driver (
     DriverId SERIAL PRIMARY KEY,
     DriverRating DECIMAL(3, 2),
@@ -34,26 +24,21 @@ CREATE TABLE Ride (
 
 -- Create the Payment table
 CREATE TABLE Payment (
-    PaymentID SERIAL PRIMARY KEY,
-    PaymentMethod VARCHAR(50) NOT NULL,
-    PaymentStatus VARCHAR(50) NOT NULL,
-    Price DECIMAL(10, 2) NOT NULL,
-    RiderId INT,
-    FOREIGN KEY (RiderId) REFERENCES "User" (UserId)
+    PaymentId INT PRIMARY KEY,
+    PaymentMethod VARCHAR(50),
+    PaymentStatus VARCHAR(20),
+    Price DECIMAL(8, 2),
+    UserId INT,
+    FOREIGN KEY (UserId) REFERENCES User(UserId),
+    RideId INT,
+    FOREIGN KEY (RideId) REFERENCES Ride(RideId)
 );
 
--- Table to delete
-CREATE TABLE "delete-table" (
-    id SERIAL PRIMARY KEY,
-    description VARCHAR(255)
+CREATE TABLE User (
+    UserId INT PRIMARY KEY,
+    Email VARCHAR(255),
+    Name VARCHAR(255),
+    Password VARCHAR(255),
+    PhoneNumber VARCHAR(15),
+    Role VARCHAR(50)
 );
-
-
-DROP TABLE "delete-table";
-
-
-
-
-ALTER TABLE Driver
-RENAME COLUMN VehicleDetails TO VehicleInfo;
-
