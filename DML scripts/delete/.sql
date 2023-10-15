48ae6d1360c0dfa0e684b505ac87d2d25c693e09
Balaji S <balajisr000@gmail.com>
2023-10-15 08:18:41 +0530

    explaination for DML delete

--- DML scripts/delete/.sql
+++ DML scripts/delete/.sql
@@ -12,3 +12,13 @@ WHERE UserId = 4;
 DELETE FROM "User"
 WHERE UserId = 4;
 
+-- optionally
+-- 1.
+-- Adjust the Foreign Key Constraint
+-- You can modify the foreign key constraint in the "Ride" table to allow cascading deletes. However, this should be done with caution, as it will automatically delete the related records in the "Ride" table when the user is deleted. Here's how to define a cascading foreign key 
+
+-- 2. 
+--Update the Foreign Key Value
+-- If it's appropriate to do so, you can update the foreign key values in the "Ride" table to a different user or set them to NULL (if allowed) before deleting the user
+
+
