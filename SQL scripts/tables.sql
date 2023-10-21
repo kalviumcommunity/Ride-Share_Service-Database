16f2577d99c57567014799d29eebb3e1c50b4497
Balaji S <balajisr000@gmail.com>
2023-10-21 17:42:49 +0530

    wip

--- SQL scripts/tables.sql
+++ SQL scripts/tables.sql
@@ -1,17 +1,12 @@
 -- Create the Ride table
-CREATE TABLE Ride (
-    RideID SERIAL PRIMARY KEY,
-    DriverId INT,
-    RideStartTime TIMESTAMP NOT NULL,
-    RideEndTime TIMESTAMP NOT NULL,
-    RideStatus VARCHAR(50) NOT NULL,
-    UserId INT,
-    PickupLocation VARCHAR(255) NOT NULL,
-    DropoffLocation VARCHAR(255) NOT NULL,
-    FOREIGN KEY (DriverId) REFERENCES Driver (DriverId),
-    FOREIGN KEY (UserId) REFERENCES "User" (UserId)
+CREATE TABLE "User" (
+    UserId SERIAL PRIMARY KEY,
+    Email VARCHAR(255) NOT NULL,
+    Name VARCHAR(255) NOT NULL,
+    Password VARCHAR(255) NOT NULL,
+    PhoneNumber VARCHAR(20) NOT NULL,
+    Role VARCHAR(50) NOT NULL
 );
-
 -- Create the Driver table
 CREATE TABLE Driver (
     DriverId SERIAL PRIMARY KEY,
@@ -48,12 +43,12 @@ CREATE TABLE Payment (
 );
 
 
+
 -- Table to delete
 CREATE TABLE "delete-table" (
     id SERIAL PRIMARY KEY,
     description VARCHAR(255)
 );
-
 
 
 DROP TABLE "delete-table";
