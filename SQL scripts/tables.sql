c17964910c10aada2387101b73194e1ee62078ca
Balaji S <balajisr000@gmail.com>
2023-10-21 17:30:28 +0530

    functional exec

--- SQL scripts/tables.sql
+++ SQL scripts/tables.sql
@@ -1,43 +1,48 @@
-CREATE TABLE Driver (
-    DriverId INT PRIMARY KEY,
-    DriverRating DECIMAL(3, 2), -- Adjusted precision and scale
-    Name VARCHAR(255),
-    PhoneNo VARCHAR(15),
-    VehicleInfo VARCHAR(255),
-    UserId INT,
-    FOREIGN KEY (UserId) REFERENCES User(UserId)
-);
-
+-- Create the Ride table
 CREATE TABLE Ride (
-    RideId INT PRIMARY KEY,
+    RideID SERIAL PRIMARY KEY,
     DriverId INT,
-    Price DECIMAL(8, 2),
-    RideEndTime DATETIME,
-    RideStartTime DATETIME,
-    RideStatus VARCHAR(20),
+    RideStartTime TIMESTAMP NOT NULL,
+    RideEndTime TIMESTAMP NOT NULL,
+    RideStatus VARCHAR(50) NOT NULL,
     UserId INT,
-    PickupLocation VARCHAR(255),
-    DropoffLocation VARCHAR(255),
-    FOREIGN KEY (DriverId) REFERENCES Driver(DriverId),
-    FOREIGN KEY (UserId) REFERENCES User(UserId)
+    PickupLocation VARCHAR(255) NOT NULL,
+    DropoffLocation VARCHAR(255) NOT NULL,
+    FOREIGN KEY (DriverId) REFERENCES Driver (DriverId),
+    FOREIGN KEY (UserId) REFERENCES "User" (UserId)
 );
 
+-- Create the Driver table
+CREATE TABLE Driver (
+    DriverId SERIAL PRIMARY KEY,
+    DriverRating DECIMAL(3, 2),
+    Name VARCHAR(255) NOT NULL,
+    PhoneNo VARCHAR(20) NOT NULL,
+    UserId INT,
+    VehicleDetails VARCHAR(255) NOT NULL,
+    FOREIGN KEY (UserId) REFERENCES "User" (UserId)
+);
+
+-- Create the Ride table
+CREATE TABLE Ride (
+    RideID SERIAL PRIMARY KEY,
+    DriverId INT,
+    RideStartTime TIMESTAMP NOT NULL,
+    RideEndTime TIMESTAMP NOT NULL,
+    RideStatus VARCHAR(50) NOT NULL,
+    UserId INT,
+    PickupLocation VARCHAR(255) NOT NULL,
+    DropoffLocation VARCHAR(255) NOT NULL,
+    FOREIGN KEY (DriverId) REFERENCES Driver (DriverId),
+    FOREIGN KEY (UserId) REFERENCES "User" (UserId)
+);
+
+-- Create the Payment table
 CREATE TABLE Payment (
-    PaymentId INT PRIMARY KEY,
-    PaymentMethod VARCHAR(50),
-    PaymentStatus VARCHAR(20),
-    Price DECIMAL(8, 2),
-    UserId INT,
-    FOREIGN KEY (UserId) REFERENCES User(UserId),
-    RideId INT,
-    FOREIGN KEY (RideId) REFERENCES Ride(RideId)
-);
-
-CREATE TABLE User (
-    UserId INT PRIMARY KEY,
-    Email VARCHAR(255),
-    Name VARCHAR(255),
-    Password VARCHAR(255),
-    PhoneNumber VARCHAR(15),
-    Role VARCHAR(50)
+    PaymentID SERIAL PRIMARY KEY,
+    PaymentMethod VARCHAR(50) NOT NULL,
+    PaymentStatus VARCHAR(50) NOT NULL,
+    Price DECIMAL(10, 2) NOT NULL,
+    RiderId INT,
+    FOREIGN KEY (RiderId) REFERENCES "User" (UserId)
 );
