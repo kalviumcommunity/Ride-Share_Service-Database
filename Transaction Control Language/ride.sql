cbdab350468b2c531af13c98dbaa9009fd7bd4e4
Balaji S <balajisr000@gmail.com>
2023-10-22 15:47:25 +0530

    exec `TCL`

---
+++ Transaction Control Language/ride.sql
@@ -0,0 +1,16 @@
+BEGIN;
+
+-- create a checkpoint
+
+SAVEPOINT ride_request_savepoint;
+
+INSERT INTO Ride (DriverId, RideStartTime, RideEndTime, RideStatus, UserId, PickupLocation, DropoffLocation)
+VALUES (999, '2023-10-22 10:00:00', '2023-10-22 11:00:00', 'Pending', 1, 'Location A', 'Location B');
+
+-- wait for payment to be processed
+
+-- if sussessful, commit the transaction    
+COMMIT;
+
+-- if not, rollback to the savepoint
+ROLLBACK TO SAVEPOINT ride_request_savepoint;
